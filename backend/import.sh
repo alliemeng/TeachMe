1112ee374910975846093578b9615f2bc1a56343
Allie Meng <awm79@cornell.edu>
2015-11-07 16:10:24 -0500

    Import as JSON array

--- backend/import.sh
+++ backend/import.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mongoimport --db teachme --collection users --drop --file seed.json
+mongoimport --db teachme --collection users --drop --file seed.json --jsonArray
 mongo teachme import.js
