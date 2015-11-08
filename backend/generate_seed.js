4ab1d3284d9704e15fff26d28124e11a5e187ddc
Allie Meng <awm79@cornell.edu>
2015-11-08 08:42:29 -0500

    Add more seeds

--- backend/generate_seed.js
+++ backend/generate_seed.js
@@ -11,13 +11,15 @@ var skills = {
     'Logic': {"name": "Logic", "suggestedPoints": rand(12, 22), "upvotes": rand(10, 13), "image": "http://funbuzzindia.com/wp-content/uploads/2014/11/Logical-Thinking-Head.jpg"},
     'Looking good': {"name": "Looking good", "suggestedPoints": rand(10, 15), "upvotes": rand(3, 8), "image": "http://www.imprevu.us/wp-content/uploads/2015/10/Fashion-And-Modern-Youth.jpg"},
     'Designing': {"name": "Designing", "suggestedPoints": rand(15, 25), "upvotes": rand(5, 15), "image": "http://ecx.images-amazon.com/images/I/51jxBzU40uL._SL1000_.jpg"},
-    'Trolling': {"name": "Trolling", "suggestedPoints": rand(5, 15), "upvotes": rand(60, 70), "image": "http://img3.wikia.nocookie.net/__cb20120826123355/vssaxtonhale/images/c/c2/Troll-face.png"}
+    'Trolling': {"name": "Trolling", "suggestedPoints": rand(5, 15), "upvotes": rand(60, 70), "image": "http://img3.wikia.nocookie.net/__cb20120826123355/vssaxtonhale/images/c/c2/Troll-face.png"},
+    'Physics': {"name": "Physics", "suggestedPoints": rand(5, 15), "upvotes": rand(8, 14), "image": "http://www.norwellschools.org/cms/lib02/MA01001453/Centricity/Domain/63/Physics.jpeg"}
 };
 
 var people = [
-    {'name': 'Feifan Zhou', 'skills': ['Ruby', 'iOS', 'Javascript', 'C'], 'location': [41.3144767, -72.9306981], 'imageID': '100001324728942'},
-    {'name': 'Allie Meng', 'skills': ['Lifting', 'Logic', 'Looking good'], 'location': [41.3098349, -72.9275223], 'imageID': '1206565055'},
-    {'name': 'Haroon Ismail', 'skills': ['Designing', 'Trolling', 'Looking good'], 'location': [38.8812203, -77.0404873], 'imageID': '1313413656'}
+    {'name': 'Feifan Zhou', 'skills': ['Ruby', 'iOS', 'Javascript', 'C'], 'location': [41.3144767, -72.9306981], 'imageID': '100001324728942', 'venmoEmail': 'feifan@me.com'},
+    {'name': 'Allie Meng', 'skills': ['Lifting', 'Logic', 'Looking good'], 'location': [41.3098349, -72.9275223], 'imageID': '1206565055', 'venmoEmail': 'awm79@cornell.edu'},
+    {'name': 'Haroon Ismail', 'skills': ['Designing', 'Trolling', 'Looking good'], 'location': [38.8812203, -77.0404873], 'imageID': '1313413656', 'venmoEmail': 'hoi2@cornell.edu'},
+    {'name': 'Jordan Dotzel', 'skills': ['C', 'Physics'], 'location': [42.442318, -76.5158845], 'imageID': '100001839030433', 'venmoEmail': ''}
 ];
 
 var output = '';
@@ -36,6 +38,7 @@ people.forEach(function(person, personIndex) {
     });
     output += '],' + "\n";
     output += '"imageID": "' + person.imageID + '",' + "\n";
+    output += '"venmoEmail": "' + person.venmoEmail + '",' + "\n";
     output += '"lastLoc": {' + "\n";
     output += '"type": "Point",' + "\n";
     output += '"coordinates": [' + person.location[0] + ', ' + person.location[1] + ']' + "\n";
