0ab506fc8d4548f85486f85fa1548ff258a579aa
Feifan Zhou <feifan@me.com>
2015-11-08 06:30:38 -0500

    code

--- index.ios.js
+++ index.ios.js
@@ -7,26 +7,44 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Settings,
+  SnapshotViewIOS,
+  NavigatorIOS,
   StyleSheet,
   Text,
   View,
 } = React;
 
+//import type { NavigationContext } from 'NavigationContext';
+var SearchScreen = require('./SearchScreen');
+
 var TeachMe = React.createClass({
   render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+        style={styles.container}
+        initialRoute={{
+          title: '',
+          component: SearchScreen,
+          tintColor: "orange",
+          rightButtonTitle: "Messages",
+          onRightButtonPress: () => {
+                AlertIOS.alert(
+                  'Bar Button Action',
+                  'Recognized a tap on the bar button icon',
+                  [
+                    {
+                      text: 'OK',
+                      onPress: () => console.log('Tapped OK'),
+                    },
+                  ]
+                );
+              },
+          leftButtonTitle: "Settings",
+          onLeftButtonPress: () => {},
+        }}
+      />
+    //View> </View>
     );
   }
 });
@@ -34,20 +52,10 @@ var TeachMe = React.createClass({
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    backgroundColor: 'black',
   },
 });
 
 AppRegistry.registerComponent('TeachMe', () => TeachMe);
+
+module.exports = TeachMe;
